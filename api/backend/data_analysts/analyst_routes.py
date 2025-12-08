102886fb947427bc78f6901f29e72395015ef9e1
Caitlin Heery <caitlin05aki@gmail.com>
2025-12-08 13:25:11 -0500

    chaned analyst routes

--- api/backend/data_analysts/analyst_routes.py
+++ api/backend/data_analysts/analyst_routes.py
@@ -3,7 +3,7 @@ from backend.db_connection import db
 from mysql.connector import Error
 from flask import current_app
 
-# Create a Blueprint for NGO routes
+# Create a Blueprint for analysts routes 
 analysts = Blueprint("analysts", __name__)
 
 # list of dashboard with names, metrics displayed, and last updated timestamp
